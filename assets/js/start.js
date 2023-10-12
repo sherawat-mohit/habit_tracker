bead46d082595f3954809f4bbdcc47b080569048
Mohit Sherawat <107291432+sherawat-mohit@users.noreply.github.com>
2023-10-12 19:43:07 +0530

    Completed

---
+++ assets/js/start.js
@@ -0,0 +1,7 @@
+// IFFE Method to redirect to the home page of habits
+// {
+//     $('#startButton').click(function(event){
+//         event.preventDefault();
+//         window.location.href = "http://localhost:3000/habits/dailyView";
+//     }
+// )};
