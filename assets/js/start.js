e50a48d5907900b7a29703b32ec06875aede5fcd
Mohit Sherawat <107291432+sherawat-mohit@users.noreply.github.com>
2023-10-12 18:59:59 +0530

    Completed

--- assets/js/start.js
+++
@@ -1,7 +0,0 @@
-// IFFE Method to redirect to the home page of habits
-{
-    $('#startButton').click(function(event){
-        event.preventDefault();
-        window.location.href = "../../views/home";
-    });
-};
